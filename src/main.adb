9518e30181c14733181e3dccfd3f948a44ff5290
Alex Tsantilis <alex.tsantilis@gmail.com>
2020-02-03 01:24:40 -0500

    Tweaked placeholder main

--- src/main.adb
+++ src/main.adb
@@ -1,6 +1,7 @@
+with Ada.Text_IO; use Ada.Text_IO;
+with Substances; use Substances;
 procedure Main is
-
 begin
-   --  Insert code here.
+   Put_Line("Market Rate for Cocaine: " & Integer'Image(Get_Market_Rate(Cocaine)));
    null;
 end Main;
